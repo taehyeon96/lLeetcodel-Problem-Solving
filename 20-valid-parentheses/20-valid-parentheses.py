044aeb80bb961b68e82cafc297b5a8548b4df92c
TaeHyeon Kwon <61690289+taehyeon96@users.noreply.github.com>
2022-09-16 21:13:20 +0900

    Time: 60 ms (25.78%), Space: 13.8 MB (98.13%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -13,7 +13,7 @@
 
 ### 알고리즘 및 방법
 
-if len(s) == 1 or s[0] not in ['(', '[', '{']    # 시작할 때 예외처리
+if len(s) == 1:    # 시작할 때 예외처리
     return False
     
 스택 만들고
@@ -23,6 +23,9 @@ for i in s:
         스택.append()
         
     else:
+        if 스택이 비어잇으면(= 여는 괄호가 없는데 닫는괄호 나온 경우) 중요
+            return false
+            
         open = 스택.pop()
         if open == '(' and (i == ']' or i == '}'):
             return False
@@ -35,30 +38,50 @@ return True
 class Solution:
     def isValid(self, s: str) -> bool:
         
-        if len(s) == 1:    # 시작할 때 예외처리
-            return False
+#         if len(s) == 1:    # 시작할 때 예외처리
+#             return False
     
-        stack = []
+#         stack = []
 
-        for i in s:
-            if i in ['(', '[', '{']:
-                stack.append(i)
+#         for i in s:
+#             if i in ['(', '[', '{']:
+#                 stack.append(i)
                 
-            else:
-                if not stack:
-                    return False
+#             else:
+#                 if not stack:
+#                     return False
                 
-                nono = stack.pop()
-                if nono == '(' and (i == ']' or i == '}'):
-                    return False
-                elif nono == '[' and (i == ')' or i == '}'):
-                    return False
-                elif nono == '{' and (i == ')' or i == ']'):
-                    return False
+#                 nono = stack.pop()
+#                 if nono == '(' and (i == ']' or i == '}'):
+#                     return False
+#                 elif nono == '[' and (i == ')' or i == '}'):
+#                     return False
+#                 elif nono == '{' and (i == ')' or i == ']'):
+#                     return False
         
-        if stack:
-            return False
-        else:
-            return True        
+#         if stack:
+#             return False
+#         else:
+#             return True      
+        
+        # 솔루션 코드 - 책정답
+        stack = []
+        # "여는 괄호"를 value로 --> if A in arr 할 때 활용하기 위함
+        table = {
+            ')' : '(',
+            ']' : '[',
+            '}' : '{'
+        }
+        
+        for c in s:
+            if c not in table:
+                stack.append(c)
+                
+            # 스택이 비어있거나(여는게 없는데 닫는게 나옴) or 스택pop이 매칭되지 않으면 false
+            elif not stack or table[c] != stack.pop():
+                return False
+        return len(stack) == 0
+            
+            
     
     
