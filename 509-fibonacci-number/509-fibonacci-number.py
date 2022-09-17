518f5cea1e836e982e1d33cdaa3462314741dbd6
TaeHyeon Kwon <61690289+taehyeon96@users.noreply.github.com>
2022-09-17 23:36:28 +0900

    Time: 61 ms (39.62%), Space: 14 MB (9.54%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -6,21 +6,22 @@ class Solution:
     
     def fib(self, n: int) -> int:
         
-        # 2번) 메모이제이션 (top-down) : 위에서부터 최고끝까지 내려간 후 하나씩 계산해 올라가되, 이미 있는 값은 계산X
+        # 1번) 타뷸레이션 (bottom-up) : 테이블을 앞으로 만들어나가는 방법
         
-        if n <= 1:  
-            return n         # 0과 1은 인덱스넘버로 리턴해야함
-        if arr[n] != 0:      # 값이 있으면
-            return arr[n]
+        arr[0], arr[1] = 0, 1
         
-        arr[n] = self.fib(n-1) + self.fib(n-2)   # (중요) 클래스 안 재귀함수 사용법
-        
+        for i in range(2, n+1):
+            arr[i] = arr[i-2] + arr[i-1]
         return arr[n]
+
+
+#         # 2번) 메모이제이션 (top-down) : 위에서부터 최고끝까지 내려간 후 하나씩 계산해 올라가되, 이미 있는 값은 계산X
         
-#         # 1번) 타뷸레이션 (bottom-up) : 테이블을 앞으로 만들어나가는 방법
-#         
-#         arr[0], arr[1] = 0, 1
+#         if n <= 1:  
+#             return n         # 0과 1은 인덱스넘버로 리턴해야함
+#         if arr[n] != 0:      # 값이 있으면
+#             return arr[n]
         
-#         for i in range(2, n+1):
-#             arr[i] = arr[i-2] + arr[i-1]
+#         arr[n] = self.fib(n-1) + self.fib(n-2)   # (중요) 클래스 안 재귀함수 사용법
+        
 #         return arr[n]
