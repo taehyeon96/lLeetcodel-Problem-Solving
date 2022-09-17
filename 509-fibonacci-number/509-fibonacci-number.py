b321b33f028967940140bb35b8c26b74a2fe4af8
TaeHyeon Kwon <61690289+taehyeon96@users.noreply.github.com>
2022-09-17 23:55:31 +0900

    Time: 46 ms (68.76%), Space: 13.8 MB (55.18%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -12,22 +12,22 @@ class Solution:
 #             x, y = y, x + y
 #         return x
         
-        # 1번) 타뷸레이션 (bottom-up) : 테이블을 앞으로 만들어나가는 방법
+#         # 1번) 타뷸레이션 (bottom-up) : 테이블을 앞으로 만들어나가는 방법
         
-        arr[0], arr[1] = 0, 1
+#         arr[0], arr[1] = 0, 1
         
-        for i in range(2, n+1):
-            arr[i] = arr[i-2] + arr[i-1]
-        return arr[n]
-
-
-#         # 2번) 메모이제이션 (top-down) : 위에서부터 최고끝까지 내려간 후 하나씩 계산해 올라가되, 이미 있는 값은 계산X
-        
-#         if n <= 1:  
-#             return n         # 0과 1은 인덱스넘버로 리턴해야함
-#         if arr[n] != 0:      # 값이 있으면
-#             return arr[n]
-        
-#         arr[n] = self.fib(n-1) + self.fib(n-2)   # (중요) 클래스 안 재귀함수 사용법
-        
+#         for i in range(2, n+1):
+#             arr[i] = arr[i-2] + arr[i-1]
 #         return arr[n]
+
+
+        # 2번) 메모이제이션 (top-down) : 위에서부터 최고끝까지 내려간 후 하나씩 계산해 올라가되, 이미 있는 값은 계산X
+        
+        if n <= 1:  
+            return n         # 0과 1은 인덱스넘버로 리턴해야함
+        if arr[n] != 0:      # 값이 있으면
+            return arr[n]
+        
+        arr[n] = self.fib(n-1) + self.fib(n-2)   # (중요) 클래스 안 재귀함수 사용법
+        
+        return arr[n]
